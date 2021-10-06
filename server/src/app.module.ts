db78aaddaed1f60f58fee94521f1665323a8e5a7
Micah D <micah.git@dvyld.com>
2021-10-06 12:11:20 +0200

    feat(server): add mock search endpoint PST-17

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MockPatentsService } from './mock-patents.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [AppService, MockPatentsService],
 })
 export class AppModule {}
