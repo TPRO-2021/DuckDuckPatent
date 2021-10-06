568ef2d3b15eb163eb7df54b632a300c9f77a99a
Samuel Hübner <samu.huebner@gmail.com>
2021-10-06 16:48:09 +0200

    feat(client): Add basic button - PST-36

---
+++ client/src/components/Button.vue
@@ -0,0 +1,56 @@
+<template>
+    <button class="button" @click="$emit('onClicked')">
+        <!-- The icon-->
+        <span class="btn-icon material-icons">{{ iconKey }}</span>
+        <!-- The text -->
+        <span class="btn-text">{{ btnText }}</span>
+    </button>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    props: {
+        btnText: String,
+        iconKey: String,
+    },
+    emits: ['onClicked'],
+})
+
+/**
+ * Simple component which provides a button.
+ *
+ * The button text can be modified with the property btnText.
+ * Additionally an icon can be passed to the button using the iconKey value.
+ *
+ * This component uses google icons
+ *
+ * Once the button is pressed it emits an event called 'onClicked'
+ * which can be handled by the parent component using @on-clicked="handler"
+ *
+ * */
+export default class Button extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #000000;
+    border-radius: 90px;
+    border: none;
+    min-height: 26px;
+}
+
+.btn-icon {
+    color: white;
+    height: 24px;
+}
+
+.btn-text {
+    color: white;
+    margin: 5px 7px;
+}
+</style>
