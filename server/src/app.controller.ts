b0bd32f74234e6a0526f16e7d7daee32350842fb
Micah D <micah.git@dvyld.com>
2021-10-06 12:11:20 +0200

    feat(server): Add mock search endpoint - PST-17

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MockPatentsService } from './mock-patents.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService, private readonly patentsService: MockPatentsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Post('/search')
+    search(@Body() body: any): any[] {
+        return this.patentsService.search(body.terms);
+    }
 }
