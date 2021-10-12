2ed5beee6f7354e4a64159b9ae90595abcb98ad2
duaa <duaakelzi@gmail.com>
2021-10-12 21:34:55 +0200

    feat(server): create mock api endpoint for keyword suggestions - PST-53

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatentsModule } from './patents/patents.module';
+import { KeywordModule } from './keyword/keyword.module';
 
 @Module({
-    imports: [PatentsModule],
+    imports: [PatentsModule, KeywordModule],
     controllers: [AppController],
     providers: [AppService],
 })
