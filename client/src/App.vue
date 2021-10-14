7817be63c600c2cbe5d1d91f372e84896ea0b26b
duaa <duaakelzi@gmail.com>
2021-10-14 14:11:33 +0200

    feat(client): Add font Saira to the project - PST-57

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,7 +6,6 @@
 @import '~@/styles/global.scss';
 
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
@@ -14,13 +13,6 @@
 }
 
 /* You can add global styles to this file, and also import other style files */
-
-// removes default padding
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
 
 // removes default border from button
 button {
