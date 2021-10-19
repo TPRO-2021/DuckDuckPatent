b569a11ff37047ab7b82aec9c09aca1498d53e4f
Samuel Hübner <samu.huebner@gmail.com>
2021-10-19 12:36:48 +0200

    feat(client): Add vuex shims to support typescript - PST-59

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,31 @@
 import { createStore } from 'vuex';
 
+export class AppState {
+    /**
+     * True if the loading screen should be visible
+     */
+    public showLoadingScreen = false;
+}
+
 export default createStore({
-    state: {},
-    mutations: {},
+    state: new AppState(),
+    mutations: {
+        /**
+         * Sets the loading screen to visible
+         * @param state
+         */
+        showLoadingScreen(state) {
+            state.showLoadingScreen = true;
+        },
+
+        /**
+         * Hides the loading screen
+         * @param state
+         */
+        hideLoadingScreen(state) {
+            state.showLoadingScreen = false;
+        },
+    },
     actions: {},
     modules: {},
 });
