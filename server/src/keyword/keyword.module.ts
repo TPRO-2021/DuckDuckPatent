346aab2c3dd3f6d40982b700fc4d657b287ed08a
Micah D <micah.git@dvyld.com>
2021-10-20 14:28:50 +0200

    feat(server): add call to real suggestion API - PST-58

--- server/src/keyword/keyword.module.ts
+++ server/src/keyword/keyword.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KeywordController } from './keyword.controller';
 import { KeywordService } from './keyword.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+    imports: [HttpModule],
     controllers: [KeywordController],
     providers: [KeywordService],
 })
--- server/src/keyword/keyword.service.ts
+++ server/src/keyword/keyword.service.ts
@@ -1,11 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import { SuggestionAPIResponse } from '../models';
 
-const mockKeywords = ['grain', 'water', 'bread', 'hunger'];
+const mockKeywords: SuggestionAPIResponse = [
+    ['grain', 0.5],
+    ['water', 0.5],
+    ['bread', 0.4],
+    ['hunger', 0.3],
+];
+
+// How many keywords we want to return
+const MAX_KEYWORDS = 6;
 
 @Injectable()
 export class KeywordService {
-    getSuggestions(keywords: string[] = []): any[] {
-        // TODO: Implement the fetching of keywords from the backend
-        return mockKeywords;
+    constructor(private readonly httpService: HttpService) {}
+
+    /* Get suggestions for terms */
+    async getSuggestions(keywords: string[] = []): Promise<string[]> {
+        // The number of suggestions that we should display should be tempered by the number of keywords given
+        // This is a simple algorithm, but we could reach for complex ones later as needed
+        const number = Math.max(3, MAX_KEYWORDS - keywords.length);
+
+        // Wrapping all requests up in Promise.all allows all of the requests to run simultaneously
+        // and only return when done
+        const r = await Promise.all(
+            keywords
+                .filter((t) => t) // Filter out null, 0, and empty string
+                .map((word) => {
+                    // We're defaulting to english for now, but that could change in the future
+                    const url = `${process.env.SUGGESTIONS_API_URL}/similarities?word=${word}&number=${number}&language=en`;
+
+                    // Return that in a promise the last value from the request stream, return that in a promise
+                    return lastValueFrom(this.httpService.get<SuggestionAPIResponse>(url))
+                        .then((t) => t.data)
+                        .catch((error) => {
+                            // Calls to this API might fail
+
+                            // Check if we're in Production, if so we need to just fail - showing mock results is no-go!
+                            if (process.env.NODE_ENV === 'production') {
+                                // Rethrowing the error breaks out of the catch block
+                                throw error;
+                            }
+                            console.log('Error calling Suggestion API:', error && error.message, url);
+                            // In development, it might not make sense to always be running the suggestion API
+                            // because it requires quite a bit of RAM + CPU.
+                            // Therefore we return mock results so that the UI can still be developed
+                            return mockKeywords;
+                        });
+                }),
+        );
+
+        // Here we combine all the returned arrays, and flatten the results into a object/map
+        const flattenedResults = r
+            .reduce((a, b) => [...a, ...b], []) // Combine into one big array
+            .reduce((a, b) => ({ ...a, [b[0]]: b[1] }), {}); // Remove duplicates by smashing into a map
+
+        return Object.keys(flattenedResults) // Get the terms (the keys of the map)
+            .sort((a, b) => flattenedResults[b] - flattenedResults[a]) // Sort in descending order
+            .filter((t) => !keywords.includes(t)) // Don't count the ones we started with
+            .slice(0, MAX_KEYWORDS); // Only return the top-n
     }
 }
