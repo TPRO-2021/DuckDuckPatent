3eb0f028396c588b84d3769c441d7e46372ce20e
Fiordi <fiordi_001@hotmail.com>
2021-11-05 20:32:30 +0100

    feat(client): Added the filter date range validation - PST-105

--- client/src/services/filter-helper.service.ts
+++ client/src/services/filter-helper.service.ts
@@ -1,25 +1,25 @@
 import { Filter } from '@/models/filter';
 
 export default class FilterHelperService {
-    static isValid(filter:Filter): boolean {
-
+    static isValid(filter: Filter): boolean {
         let year1, year2;
         switch (filter.type) {
             case 'date':
-                [year1, year2] = filter.value.split('-').map(t => parseInt(t, 10));
-                return FilterHelperService.isValidYear(year1) && FilterHelperService.isValidYear(year2) && year2 > year1;
+                [year1, year2] = filter.value.split('-').map((t) => parseInt(t, 10));
+                return (
+                    FilterHelperService.isValidYear(year1) && FilterHelperService.isValidYear(year2) && year2 > year1
+                );
             case 'empty':
                 return false;
-            default: return !!filter.value;
+            default:
+                return !!filter.value;
         }
     }
     static isValidYear(year: number) {
-                return !isNaN(year) && year > 1700 && year <= (new Date()).getFullYear();
-        }
+        return !isNaN(year) && year > 1700 && year <= new Date().getFullYear();
+    }
 
     static getParameterList(filters: Filter[]): string[] {
-
-
         // Prep the filter get parameters
         const filterParams = filters
             .filter((filter) => FilterHelperService.isValid(filter)) // Remove unfinished or mal-formed filters
